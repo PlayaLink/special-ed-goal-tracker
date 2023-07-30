11438c0d8f352a5e423df729a7355157e27acf2c
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2023-07-29 23:05:49 -0400

    Passses api keys directly, without using env variables

--- firebase/config.js
+++ firebase/config.js
@@ -2,13 +2,13 @@
 import { initializeApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD0gNTjdI4cFXjLnG3v1n56yJSeDKL2qrM",
+  authDomain: "iep-goal-tracker-64fbc.firebaseapp.com",
+  projectId: "iep-goal-tracker-64fbc",
+  storageBucket: "iep-goal-tracker-64fbc.appspot.com",
+  messagingSenderId: "126940751503",
+  appId: "1:126940751503:web:af4230cf5860832128b2fa",
+  measurementId: "G-S75D59L9DG"
 };
 
 // Initialize Firebase
